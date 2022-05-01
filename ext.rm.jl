f7eb60b66886fd97dce12d5d76f6ab472ce1ac8f
Heptazhou <zhou@0h7z.com>
2022-05-01 03:44:50 +0000

    Update

--- ext.rm.jl
+++ ext.rm.jl
@@ -3,21 +3,24 @@ try
 	for p in [
 		#
 		"ajjwvijx" => ".m4a",
-		"m4a" => ".m4a",
-		"qnxy" => ".m4a",
+		"m4a"      => ".m4a",
+		"qnxy"     => ".m4a",
 	]
-		cd(p[1])
-		for f in readdir()
-			splitext(f)[2] in [".js", ".txt"] && continue
-			splitext(f)[2] == p[2] || continue
-			mv(f, splitext(f)[1])
-			global n += 1
+		cd(p[1]) do
+			for f in readdir()
+				splitext(f)[2] in [".js", ".txt"] && continue
+				splitext(f)[2] == p[2] || continue
+				mv(f, splitext(f)[1])
+				global n += 1
+			end
 		end
-		cd("..")
 	end
 	@info "完成 > $n"
 catch e
 	@info "错误 > $n"
 	@info e
 end
+isempty(ARGS) || exit()
+print("> ")
 readline()
+
